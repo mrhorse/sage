5ff85267ab4c8014d76adc21b436786bd0282e49
Steve Horsey <steve@thisisyoke.com>
2016-01-20 18:28:40 +0000

    Changed the way page template works

--- page.php
+++ page.php
@@ -1,4 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+   <?php 
+    // Just duplicate this template, rename to page-case-studies.php for example, then do the business in templates/content-page-case-studies.php
+    $page_template = get_page_template_slug() !== '' ?  str_replace('.php', '', get_page_template_slug() : 'page' ); // get the partial for this template, e.g. page-case-studies
+    get_template_part('templates/content', $page_template); 
+  ?>
 <?php endwhile; ?>
--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,3 @@
+<?php get_template_part('templates/page', 'header'); ?>
 <?php the_content(); ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
